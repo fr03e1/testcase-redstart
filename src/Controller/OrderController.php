e0d80a47710b87582b0d12abfa4d6c6a7056a501
fr03e1 <aleksejbocarov65@gmail.com>
2023-06-02 19:28:58 +0300

    add Dto and entity

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,32 +2,50 @@
 
 namespace App\Controller;
 
+use App\Dto\ItemDto;
+use Symfony\Component\HttpFoundation\Response;
+use App\Services\OrderService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class OrderController extends AbstractController
 {
+    public function __construct(private OrderService $orderService)
+    {
+    }
 
-    #[Route('/order', name: 'order.index')]
+    #[Route('/orders', name: 'order.index', methods: ['GET','HEAD'])]
     public function index(): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/OrderController.php',
-        ]);
+        $data = $this->orderService->getAllOrders();
+        return new JsonResponse(data: $data , status: Response::HTTP_OK);
     }
 
-    #[Route('/order', name: 'order.store',methods: 'POST')]
-    public function store(): JsonResponse
+    #[Route('/orders/{id}', name: 'order.show', methods: ['GET','HEAD'])]
+    public function show(int $id): JsonResponse
     {
-        return $this->json([
-                'message' => 'Welcome to your new controller!',
-                'path' => 'src/Controller/OrderController.php',
-        ]);
+        return $this->json($this->orderService->getOrder($id));
     }
 
-    #[Route('/order/{order}', name: 'order.update',methods: 'PATCH')]
+    #[Route('/orders', name: 'order.store', methods: 'POST')]
+    public function store(
+            #[MapRequestPayload]  ItemDto $itemDto,
+                                  ValidatorInterface $validator
+    ): JsonResponse
+    {
+        $errors = $validator->validate($itemDto);
+
+        if (count($errors)>0) {
+           return$this->json((string) $errors);
+        }
+
+        return $this->json(data: $itemDto);
+    }
+
+    #[Route('/order/{id}', name: 'order.update',methods: 'PATCH')]
     public function update(): JsonResponse
     {
         return $this->json([
@@ -36,7 +54,7 @@ class OrderController extends AbstractController
         ]);
     }
 
-    #[Route('/order/{order}', name: 'order.delete',methods: 'DELETE')]
+    #[Route('/order/{id}', name: 'order.delete',methods: 'DELETE')]
     public function delete(): JsonResponse
     {
         return $this->json([
